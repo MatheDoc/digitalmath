33ded49617424e00a10a2653f58a3a702baca8ab
MatheDoc <sven.fuehring@gmail.com>
2024-11-12 22:50:21 +0100

    a

--- scripts/buttonHandlers.js
+++ scripts/buttonHandlers.js
@@ -17,81 +17,74 @@ function addButtons(){
         }
         table.cell(rowIdx, 5).data(ichKannText + ' ' + infoButton + ' ' + copyButton + ' ' + testButton);
     });
-}
 
-// Event-Listener für die Info-Buttons
-$('#meineTabelle').on('click', '.info-icon', function(event) {
-event.stopPropagation(); // Verhindert, dass das Overlay direkt den Klick abfängt
-var row = $(this).closest('tr'); // Zeile ermitteln
-var rowData = $('#meineTabelle').DataTable().row(row).data(); // Zeilendaten ermitteln
-var info = rowData[7]; // Infos in der 8. Spalte
-
-// Infotext aktualisieren
-$('#infoText').html(info);
-
-// Überprüfe die Fensterbreite, um die Position zu bestimmen
-if ($(window).width() > 768) { // Für Desktop
-    // Positioniere den Infotext relativ zum Icon
-    var iconOffset = $(this).offset(); // Position des Icons
-    $('#infoText').css({
-        top: iconOffset.top + $(this).height() + 5, // 5px Abstand unter dem Icon
-        left: iconOffset.left
-    }).fadeIn(); // Zeige den Infotext an
-} else { // Für mobile Geräte
-    // Info-Box zentrieren
-    $('#infoText').css({
-        top: '50%', 
-        left: '50%',
-        transform: 'translate(-50%, -50%)', // Zentrierung
-        position: 'fixed' // Sicherstellen, dass sie fest bleibt
-    }).fadeIn(); // Zeige den Infotext an
-}
-$('#overlay').fadeIn();   // Zeige das Overlay an
-
-});
-
-// Ausblenden des Infotextes beim Klicken irgendwo anders auf die Seite
-$(document).on('click', function() {
-    $('#infoText').fadeOut(); // Verstecke den Infotext
-    $('#overlay').fadeOut();  // Verstecke das Overlay
-});
-
-// Verhindern, dass der Infotext ausgeblendet wird, wenn man darauf klickt
-$('#infoText').on('click', function(event) {
-    event.stopPropagation(); // Verhindert das Ausblenden beim Klick auf den Infotext
-});
-
-
-// Event-Listener für die Test-Buttons
-$('#meineTabelle').on('click', '.test-icon', function() {
+    // Event-Listener für die Info-Buttons
+    $('#meineTabelle').on('click', '.info-icon', function(event) {
+    event.stopPropagation(); // Verhindert, dass das Overlay direkt den Klick abfängt
     var row = $(this).closest('tr'); // Zeile ermitteln
     var rowData = $('#meineTabelle').DataTable().row(row).data(); // Zeilendaten ermitteln
-    var urls = rowData[6]; // URLs in der 7. Spalte
-    var ichKannText = 'Ich kann ' + rowData[5].split('<i')[0].trim(); //Ich kann text alt Titel
-    
-    if (urls) {
-        // Teile die URLs und erstelle Links
-        var links = urls.split(',').map(function(url) {
-            var sammlungsname = url.trim() + '.json';
-            return 'sammlung=' + sammlungsname;
-        });
-        var testUrl = 'https://mathedoc.github.io/digitalmath/quiz.html?' + links.join('&') + '&titel=' + encodeURIComponent(ichKannText);
-        window.open(testUrl, '_blank'); // Öffne URL in einem neuen Tab/Fenster
+    var info = rowData[7]; // Infos in der 8. Spalte
+    // Infotext aktualisieren
+    $('#infoText').html(info);
+    // Überprüfe die Fensterbreite, um die Position zu bestimmen
+    if ($(window).width() > 768) { // Für Desktop
+        // Positioniere den Infotext relativ zum Icon
+        var iconOffset = $(this).offset(); // Position des Icons
+        $('#infoText').css({
+            top: iconOffset.top + $(this).height() + 5, // 5px Abstand unter dem Icon
+            left: iconOffset.left
+        }).fadeIn(); // Zeige den Infotext an
+    } else { // Für mobile Geräte
+        // Info-Box zentrieren
+        $('#infoText').css({
+            top: '50%', 
+            left: '50%',
+            transform: 'translate(-50%, -50%)', // Zentrierung
+            position: 'fixed' // Sicherstellen, dass sie fest bleibt
+        }).fadeIn(); // Zeige den Infotext an
     }
-});
-
-
-// Event-Listener für die Copy-Buttons
-$('#meineTabelle').on('click', '.copy-icon', function() {
-    var row = $(this).closest('tr');
-    var text1 =  table.cell(row, 5).data().split('<i')[0].trim();
-    var text2 = table.cell(row, 7).data();
-    var textToCopy = 'Ich kann ' + text1;
-    if(text2){
-        textToCopy += ' (' + text2 + ')'
-    }
-    // Kopieren in die Zwischenablage
-    navigator.clipboard.writeText(textToCopy).then(() => {
-        // Meldung
+    $('#overlay').fadeIn();   // Zeige das Overlay an
     });
-});
+    // Ausblenden des Infotextes beim Klicken irgendwo anders auf die Seite
+    $(document).on('click', function() {
+        $('#infoText').fadeOut(); // Verstecke den Infotext
+        $('#overlay').fadeOut();  // Verstecke das Overlay
+    });
+    // Verhindern, dass der Infotext ausgeblendet wird, wenn man darauf klickt
+    $('#infoText').on('click', function(event) {
+        event.stopPropagation(); // Verhindert das Ausblenden beim Klick auf den Infotext
+    });    
+   
+   // Event-Listener für die Test-Buttons
+    $('#meineTabelle').on('click', '.test-icon', function() {
+        var row = $(this).closest('tr'); // Zeile ermitteln
+        var rowData = $('#meineTabelle').DataTable().row(row).data(); // Zeilendaten ermitteln
+        var urls = rowData[6]; // URLs in der 7. Spalte
+        var ichKannText = 'Ich kann ' + rowData[5].split('<i')[0].trim(); //Ich kann text alt Titel    
+        if (urls) {
+            // Teile die URLs und erstelle Links
+            var links = urls.split(',').map(function(url) {
+                var sammlungsname = url.trim() + '.json';
+                return 'sammlung=' + sammlungsname;
+            });
+            var testUrl = 'https://mathedoc.github.io/digitalmath/quiz.html?' + links.join('&') + '&titel=' + encodeURIComponent(ichKannText);
+            window.open(testUrl, '_blank'); // Öffne URL in einem neuen Tab/Fenster
+        }
+    });   
+    
+    // Event-Listener für die Copy-Buttons
+    $('#meineTabelle').on('click', '.copy-icon', function() {
+        var row = $(this).closest('tr');
+        var text1 =  table.cell(row, 5).data().split('<i')[0].trim();
+        var text2 = table.cell(row, 7).data();
+        var textToCopy = 'Ich kann ' + text1;
+        if(text2){
+            textToCopy += ' (' + text2 + ')'
+        }
+        // Kopieren in die Zwischenablage
+        navigator.clipboard.writeText(textToCopy).then(() => {
+            // Meldung
+        });
+    });
+}
+
--- scripts/tableHandlers.js
+++ scripts/tableHandlers.js
@@ -46,6 +46,56 @@ function initializeTable() {
         table.draw(); // Zeichne die Tabelle neu
     });
 
+    // Teilen-Link generieren
+    $('#shareLink').on('click', function() {
+    const selectedCheckboxes = []; // Array zur Speicherung der ausgewählten Checkboxen
+
+    // Durchlaufen der ausgewählten Checkboxen
+    table.$('.rowCheckbox:checked').each(function() {
+        const checkboxIndex = table.row($(this).closest('tr')).index(); // Gibt den Index der Zeile zurück
+        selectedCheckboxes.push(checkboxIndex);
+    });
+
+    // Die Werte der ausgewählten Checkboxen in einen JSON-String umwandeln
+    const json = JSON.stringify(selectedCheckboxes);
+    const encodedJson = encodeURIComponent(json); // JSON-String URL-kodieren
+    const shareableLink = `${window.location.href}?config=${encodedJson}`; // Teilen-Link erstellen
+
+    // Link in die Zwischenablage kopieren
+    navigator.clipboard.writeText(shareableLink).then(() => {
+        alert('Link der Auswahl wurde in die Zwischenablage kopiert: ' + shareableLink);
+    }).catch(err => {
+        console.error('Fehler beim Kopieren in die Zwischenablage: ', err);
+    });
+});
+
+ // Funktion zum Auswählen/Auswählen aller Checkboxen
+ $('#checkAll').on('click', function() {
+    var rows = table.rows({ 'search': 'applied' }).nodes(); // Holt alle sichtbaren Zeilen
+    $('input.rowCheckbox', rows).prop('checked', this.checked); // Setzt den Status der Checkboxen
+});
+
+// Überprüfen, ob ein "config"-Parameter in der URL vorhanden ist
+function checkConfigParameter(){
+    const urlParams = new URLSearchParams(window.location.search);
+    const configJson = urlParams.get('config');
+    if (configJson) {
+        try {
+            const importedData = JSON.parse(decodeURIComponent(configJson));
+                // Überprüfen, ob die Checkboxen in der Tabelle gesetzt werden müssen
+                table.rows().every(function(index) { // index hier als Parameter
+            const checkbox = $(this.node()).find('td:first-child .rowCheckbox'); // Die Checkbox in der ersten Spalte finden
+            const isSelected = importedData.includes(index); // Überprüfen, ob der Zeilenindex in den importierten Daten vorhanden ist
+            checkbox.prop('checked', isSelected); // Checkbox entsprechend setzen
+        });
+        } catch (e) {
+            console.error('Ungültige Konfigurationsdaten');
+        }
+    }
+}
+
+
+
     // Funktion zum Anpassen der Suchleiste
     function adjustSearchFilter() {
         // Verschiebt das Filter-Div in den gewünschten Container
