9f8f2cab836d48cd8e8c613be8e60ebd404b10de
MatheDoc <sven.fuehring@gmail.com>
2024-11-12 22:33:21 +0100

    a

--- scripts/main.js
+++ scripts/main.js
@@ -6,18 +6,24 @@ $(document).ready(function() {
     loadHTML();
 });
 
+
+//dynamischer Pfad wegen Github - Liveserver
+const basePath = window.location.hostname.includes("github.io")
+    ? "/digitalmath"
+    : ""; // Kein Pfad bei lokalem Server
+
 // html Inhalt laden
 async function loadHTML() {
     try {
         $('#content').html('<p>Lade Daten...</p>');
-        const response = await fetch('/digitalmath/liste.html');
+        const response = await fetch(`${basePath}/liste.html`);
         const html = await response.text();
         $('#content').html(html);
-        initializeTable();
-        adjustSearchFilter();
-         setupDropdowns();
-        addButtons();
-        checkConfigParameter();
+        //initializeTable();
+        //adjustSearchFilter();
+        //setupDropdowns();
+        //addButtons();
+        //checkConfigParameter();
     } catch (error) {
         console.error('Fehler beim Laden der HTML-Datei:', error);
         $('#content').html('<p>Fehler beim Laden der Daten.</p>');
