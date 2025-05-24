fcc4a93561a14eeb5fe8d98d20706de3bcc67285
MatheDoc <sven.fuehring@gmail.com>
2025-05-24 18:43:53 +0200

    a

--- js/skriptElemente.js
+++ js/skriptElemente.js
@@ -2,11 +2,11 @@ function resizeIframe(iframe) {
   try {
     const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
     const height = iframeDoc.body.scrollHeight;
-    iframe.style.height = (height + 20) + 'px';
-    iframe.style.width = '100%';
-    iframe.style.border = 'none';
+    iframe.style.height = height + 20 + "px";
+    iframe.style.width = "100%";
+    iframe.style.border = "none";
   } catch (e) {
-    console.warn('Kein Zugriff auf iframe-Inhalt:', e);
+    console.warn("Kein Zugriff auf iframe-Inhalt:", e);
   }
 }
 
@@ -16,90 +16,88 @@ function erstelleQuiz(divId, url) {
     console.error('Div mit ID "' + divId + '" nicht gefunden.');
     return;
   }
-  
+
   container.innerHTML = `<iframe src="${url}&quizonly=true" frameborder="0" allowfullscreen style="width:100%;"></iframe>`;
-  const iframe = container.querySelector('iframe');
+  const iframe = container.querySelector("iframe");
   if (!iframe) {
-    console.error('iframe konnte nicht erstellt werden.');
+    console.error("iframe konnte nicht erstellt werden.");
     return;
   }
-  
-  iframe.addEventListener('load', () => {
+
+  iframe.addEventListener("load", () => {
     setTimeout(() => resizeIframe(iframe), 1000);
   });
 }
 
+function zeichneFunktion(funktion, xMin, xMax, divId, titel = "") {
+  const xWerte = [];
+  const yWerte = [];
+  for (let x = xMin; x <= xMax; x += 0.1) {
+    xWerte.push(x);
+    yWerte.push(funktion(x));
+  }
 
+  const data = [
+    {
+      x: xWerte,
+      y: yWerte,
+      type: "scatter",
+      mode: "lines",
+      line: { color: "blue" },
+    },
+  ];
 
-
-
-function zeichneFunktion(funktion, xMin, xMax, divId, titel = '') {
-    const xWerte = [];
-    const yWerte = [];
-    for (let x = xMin; x <= xMax; x += 0.1) {
-        xWerte.push(x);
-        yWerte.push(funktion(x));
-    }
-
-    const data = [{
-        x: xWerte,
-        y: yWerte,
-        type: 'scatter',
-        mode: 'lines',
-        line: { color: 'blue' }
-    }];
-
-   const layout = {
+  const layout = {
     title: titel,
     xaxis: {
-        title: 'x'
+      title: "x",
     },
     yaxis: {
-        title: 'y'
+      title: "y",
     },
     margin: { t: 40, r: 20, b: 40, l: 50 },
-};
+  };
 
-
-    Plotly.newPlot(divId, data, layout);
+  Plotly.newPlot(divId, data, layout);
 }
 
 // histogramme
-function zeichneDiskretesHistogramm(xWerte, yWerte, divId, titel = '') {
-    const data = [{
-        x: xWerte,
-        y: yWerte,
-        type: 'bar',
-        name: 'P(X = x)',
-        marker: {
-            color: 'rgba(54, 162, 235, 0.3)',
-            line: {
-                color: 'rgba(162, 162, 162, 0.7)', // Farbe des Rahmens
-                width: 1        // Dicke des Rahmens
-            }
-        }
-    }];
-
-    const layout = {
-        title: titel,
-        xaxis: {
-            title: 'x',
-            tickmode: 'array',
-            tickvals: xWerte
+function zeichneDiskretesHistogramm(xWerte, yWerte, divId, titel = "") {
+  const data = [
+    {
+      x: xWerte,
+      y: yWerte,
+      type: "bar",
+      name: "P(X = x)",
+      marker: {
+        color: "rgba(54, 162, 235, 0.3)",
+        line: {
+          color: "rgba(162, 162, 162, 0.7)", // Farbe des Rahmens
+          width: 1, // Dicke des Rahmens
         },
-        yaxis: {
-            title: 'P(X = x)',
-            range: [0, Math.max(...yWerte) * 1.1],
-            gridcolor: 'rgba(0, 0, 0, 0.2)' 
-        },
-        bargap: 0,
-        showlegend: false,
-        margin: { t: 100, r: 20, b: 40, l: 50 },
-        dragmode: false,
-    };
+      },
+    },
+  ];
 
-        const config = { scrollZoom: false};
+  const layout = {
+    title: titel,
+    xaxis: {
+      title: "x",
+      tickmode: "array",
+      tickvals: xWerte,
+    },
+    yaxis: {
+      title: "P(X = x)",
+      range: [0, Math.max(...yWerte) * 1.1],
+      gridcolor: "rgba(0, 0, 0, 0.2)",
+    },
+    bargap: 0,
+    showlegend: false,
+    margin: { t: 100, r: 20, b: 40, l: 50 },
+    dragmode: false,
+  };
 
+  const config = { scrollZoom: false };
 
-    Plotly.newPlot(divId, data, layout, config);
+  Plotly.newPlot(divId, data, layout, config);
 }
